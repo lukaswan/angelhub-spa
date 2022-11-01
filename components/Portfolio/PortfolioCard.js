f660cabe1859734d9c36ceade27af171fc1b3640
Lukas Wan <lukas@riodefi.com>
2022-11-01 12:10:41 +0800

    added href to portfolio card

--- components/Portfolio/PortfolioCard.js
+++ components/Portfolio/PortfolioCard.js
@@ -22,7 +22,7 @@ export default function PortfolioCard({category,projects}) {
                     <a href="#" key={idx} className={`col-sm-6 d-flex-column ${styles.projectImg}`} style={{backgroundImage:`url(${item.image})`}}>
                     {item.unicorn?<p className={`${styles.unicorn}`}><img src="/images/unicorn.png" alt="Unicorn"/> Unicorn</p>:null}
                       <div>
-                        {item.annotation?<p className={`${styles.annotation} align-self-end`}>{item.annotation}</p>:null}
+                        {/* {item.annotation?<p className={`${styles.annotation} align-self-end`}>{item.annotation}</p>:null} */}
                       </div>
                     </a>
                   </>
